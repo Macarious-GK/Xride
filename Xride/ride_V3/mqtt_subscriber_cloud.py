e5c909c64c60ce0b4aaf1245ec186e769e856d41
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-10-30 14:57:49 +0300

    updating records for location

--- Xride/ride_V3/mqtt_subscriber_cloud.py
+++ Xride/ride_V3/mqtt_subscriber_cloud.py
@@ -6,6 +6,7 @@ from .models import Car
 BROKER = "mqtt.eclipseprojects.io"
 PORT = 1883
 TOPIC = "sensor/data"
+CONFIRM_TOPIC = "confirmation"
 
 # Callback when a message is received from the broker
 def receive_message(client, userdata, message):
@@ -14,11 +15,14 @@ def receive_message(client, userdata, message):
     try:
         # Retrieve the Car instance and update temperature
         car1 = Car.objects.get(id=data['car_id'])
-        car1.temperature = data['car_temperature']
+        car1.temperature = data['value']
         car1.location_latitude = data['car_location_latitude']
         car1.location_longitude = data['car_location_longitude']
         car1.save()
-        print(f"The temperature inside the car is: {data['car_temperature']}°C.")
+        print(f"The temperature inside car {data['car_id']} is: {data['value']}°C.")
+        print(f"The car location longitude is: {data['car_location_longitude']} and location latitude is: {data['car_location_latitude']}")
+        confirmation_message = {"confirmation": "Temperature data , location longitude and location latitude received successfully"}
+        client.publish(CONFIRM_TOPIC, json.dumps(confirmation_message))
     except Car.DoesNotExist:
         print(f"Car with ID {data['car_id']} does not exist.")
     except Exception as e:
